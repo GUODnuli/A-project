a71877e1ac172c83b3152847b26ff138e0dbceb5
Zhuangxiaosen <xz.ganbaruibro@gmail.com>
2025-11-04 15:58:33 +0800

    Add feature: Mouse Motion.

--- FPSController/fps_controller.gd
+++ FPSController/fps_controller.gd
@@ -1,9 +1,24 @@
 extends CharacterBody3D
 
+@export var look_sensitivity : float = 0.006
+@export var jump_velocity := 6
+
 func _ready():
 	for child in %WorldModel.find_children("*", "VisualInstance3D"):
 		child.set_layer_mask_value(1, false)
 		child.set_layer_mask_value(2, true)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			rotate_y(-event.relative.x * look_sensitivity)
+			%Camera3D.rotate_x(-event.relative.y * look_sensitivity)
+			%Camera3D.rotation.x = clamp(%Camera3D.rotation.x, deg_to_rad(-90.0), deg_to_rad(90.0))
 
 func _physics_process(float):
 	pass
